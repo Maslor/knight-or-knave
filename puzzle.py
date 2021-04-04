df8439f2c94a6db3eec695454d4fb4106f83c2a9
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2021-04-04 16:13:29 +0100

    added knowledge bases

--- puzzle.py
+++ puzzle.py
@@ -1,32 +1,53 @@
 from logic import *
 
+A = Symbol("A said it")
 AKnight = Symbol("A is a Knight")
 AKnave = Symbol("A is a Knave")
 
+B = Symbol("B said it")
 BKnight = Symbol("B is a Knight")
 BKnave = Symbol("B is a Knave")
 
+C = Symbol("C said it")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Implication(And(AKnight, AKnave), AKnight),
+    Implication(Not(And(AKnight, AKnave)), AKnave)
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Implication(And(AKnave, BKnave), AKnight),
+    Implication(Not(And(AKnave, BKnave)), AKnave),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Biconditional(BKnight, AKnave),
+    Biconditional(BKnave, AKnight),
+    Implication(
+        Or(
+            And(AKnight, BKnight),
+            And(AKnave, BKnave)
+        ),
+        AKnight
+    ),
+    Implication(
+        Not(
+            Or(
+                And(AKnight, BKnight),
+                And(AKnave, BKnave)
+            )
+        ), AKnave
+    )
 )
 
 # Puzzle 3
@@ -35,7 +56,32 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # XOR Knight, Knave
+    Or(AKnave, AKnight),
+    Or(BKnave, BKnight),
+    Or(CKnave, CKnight),
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+    Biconditional(CKnight, Not(CKnave)),
+
+    # What they say
+    Biconditional(
+        Or(
+            AKnight,
+            AKnave
+        ),
+        AKnight
+    ),
+
+    Biconditional(
+        And(
+            And(A, BKnave),
+            CKnave
+        ),
+        BKnight
+    ),
+
+    Biconditional(AKnight, CKnight)
 )
 
 
